bf7ada285a5bed0a7a83eb060a1f7da3f58c96c7
gjwoo960101 <example@com.net>
2024-11-21 16:05:07 +0900

    102page

--- react-app/src/App2.js
+++ react-app/src/App2.js
@@ -26,35 +26,64 @@ function Body(props){
     );
 }
 
+function Create(props){
+    return(
+        <form onSubmit={event => {
+            event.preventDefault();
+            props.onChangeSave(event.target.body.value);
+        }}>
+            <p><textarea name='body' placeholder='body'></textarea></p>
+            <p><input type='submit' value='li추가'></input></p>
+        </form>
+    )
+}
+
 
 function App(){
-
+    const [mode,setMode] = useState('READ');
     const [id,setId] = useState(null);
+    const [newId,setNewId] = useState(4);
 
-    let lis = [
+    const [lis,setLis]  = useState([
         {id : 1 , body : 'li-fir-ele'},
         {id : 2 , body : 'li-sec-ele'},
         {id : 3 , body : 'li-thir-ele'}
-    ]
+    ]);
 
     let selectText = '';
-
-    lis.forEach(item=>{
-        if(item.id === Number(id)){
-            selectText = item.body;
-        }
-        }
-    );
+    if(mode === 'READ'){
+        lis.forEach(item=>{
+                if(item.id === Number(id)){
+                    selectText = item.body;
+                }
+            }
+        );
+    }else if(mode === 'CREATE'){
+        selectText = <Create onChangeSave={(_body) => {
+            const addLis = {id : newId, body: _body}
+            const newLis = [...lis];
+            newLis.push(addLis);
+            setLis(newLis);
+            setMode('READ');
+            setId(newId);
+            setNewId(newId+1);
+        }}></Create>
+    }
+    
 
     return (
-        <>
-        <Header title="헤더입니다."></Header>
-        <Body li_Body={lis} onChangEle={(_id)=>{
-            setId(_id);
-        }}></Body>
-
-        {selectText}
-        </>
+        <div>
+            <Header title="헤더입니다."></Header>
+            <Body li_Body={lis} onChangEle={(_id)=>{
+                setId(_id);
+                setMode('READ')
+            }}></Body>
+            
+            <p><button onClick={()=>{
+                setMode('CREATE')
+                }} type='button'>추가</button></p>
+            <p>{selectText}</p>
+        </div>
     );
 }
 
