cde7060144d6036104cad3aaf149e050030de85a
gjwoo960101 <example@com.net>
2024-12-18 18:02:22 +0900

    redux, ES6문법 공부중

--- es6-stu/test.js
+++ es6-stu/test.js
@@ -1,1 +1,13 @@
-console.log("test합니다.")
+
+// 생성자 함수
+function User (name,age) {
+    this.name = name;
+    this.aage = age;
+    this.sayname = function (){
+        console.log("내이름은 :",this.name)
+    }
+}
+
+let testUser = new User('lee',20);
+
+testUser.sayname();
